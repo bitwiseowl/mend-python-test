9c1c50506dcfd261bd9dc59b9a4abfef02b8f79b
bitwiseowl <utandon@proton.me>
2025-09-18 09:11:37 +0530

    Final test

---
+++ src/main/java/com/example/mend_python_test/MendPythonTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mend_python_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendPythonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MendPythonTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mend_python_test/MendPythonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mend_python_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MendPythonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
